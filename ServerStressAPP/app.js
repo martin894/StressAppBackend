4fff79e8c351f6d6f77ea709f8fb7319268a9665
root <root@rainfall.simtech.uni-stuttgart.de>
2017-01-19 11:20:48 +0100

    Add certificate file

--- ServerStressAPP/app.js
+++ ServerStressAPP/app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-
+var uuid = require('node-uuid');
+const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({ contactPoints: ['host1'] });
+client.connect(function (err) {
+  assert.ifError(err);
+});
 app.get('/', function(req, res){
     console.log('GET /')
     //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
@@ -10,13 +15,17 @@ app.get('/', function(req, res){
     res.end(html);
 });
 
-app.post('/', function(req, res){
-    console.log('POST /');
-    console.dir(req.body);
+app.post('/data', function(req, res){
+	var key = uuid.v4();
+	const query = 'INSERT INTO events (key,value) VALUES (?)*';
+	const params = [key,req.body];	
+	client.execute(query, params, { prepare: true }, function (err) {
+ 	 assert.ifError(err);
+  
+	});
+    console.log('POST /data');
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('thanks');
+    res.end('Sucessful');
 });
 
-port = 3000;
-app.listen(port);
-console.log('Listening at http://localhost:' + port)
+app.listen(3000);
