3b0ed4b87b128174dfe32745bbfc1a3ecae6e0ac
Martin Root <mail@martin-root.de>
2017-01-23 18:49:46 +0100

    Update post score request

--- ServerStressAPP/app.js
+++ ServerStressAPP/app.js
@@ -61,7 +61,7 @@ app.post('/score', function(req, res){
             //     });
             // }
          });
-        if (res ==="undefined"){
+        if (res===null){
             var query = 'INSERT INTO score (deviceid,username,value) VALUES (?,?,?)';
             var params = [req.body.deviceid,req.body.username,req.body.score];
             client.execute(query, params, { prepare: true }, function (err) {
